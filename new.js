36a7ba86ec13e138ac515e12ed792c4a3af6950a
Leon Machens <leon@machens.koeln>
2021-04-29 13:43:14 +0200

    ✨ Add new task (#10)

---
+++ new.js
@@ -0,0 +1,55 @@
+const formElement = document.querySelector(".form");
+
+function parseJSONFromLocalStorage(key, defaultValue) {
+  const json = localStorage.getItem(key);
+  if (json === null) {
+    return defaultValue;
+  }
+  const data = JSON.parse(json);
+  return data;
+}
+
+function appendToArray(item, array) {
+  return [...array, item];
+}
+
+function stringifyJSONToLocalStorage(key, value) {
+  const json = JSON.stringify(value);
+
+  localStorage.setItem(key, json);
+}
+
+function goToPage(href) {
+  location.href = href;
+}
+
+formElement.onsubmit = function (event) {
+  // Prevent the default form submit behaivor (sending data to a server an reloading page)
+  event.preventDefault();
+
+  const textInputElement = document.querySelector(".form__input");
+  const checkedDateInput = document.querySelector(
+    ".radio-group__input:checked"
+  );
+
+  if (!textInputElement.value) {
+    alert("Text Input is empty!");
+    return;
+  }
+
+  if (!checkedDateInput) {
+    alert("Radio Button not checked!");
+    return;
+  }
+
+  const task = {
+    name: textInputElement.value,
+    date: checkedDateInput.value,
+  };
+
+  const taskList = parseJSONFromLocalStorage("taskList", []);
+  const newTaskList = appendToArray(task, taskList);
+  stringifyJSONToLocalStorage("taskList", newTaskList);
+
+  goToPage("/dashboard.html");
+};
